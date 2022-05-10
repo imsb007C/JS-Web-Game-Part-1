3c0c696299f741b348b69131e1e80fdb6d155c1a
Alex <imsb005@gmail.com>
2022-05-09 22:46:34 -0700

    finish activity js-2-web-game-part-i

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, left, bottom){
+    let name = document.createElement('img');
+    name.src = src;
+    name.style.position = 'fixed';
+    name.style.left = left;
+    name.style.bottom = bottom;
+    document.body.append(name);
+    return name;
+}
+
+function newItem(src, left, bottom){
+    let item = newImage(src, left, bottom);
+
+    item.addEventListener('dblclick',function(){
+        item.remove()
+    })
+}
+newImage('assets/green-character.gif','100px','100px');
+newImage('assets/pine-tree.png', '450px', '200px');
+newImage('assets/tree.png','200px','300px');
+newImage('assets/pillar.png', '350px', '100px');
+newImage('assets/crate.png','150px','200px');
+newImage('assets/well.png','500px','425px');
+
+
+newItem('assets/sword.png', '500px', '405px');
+newItem('assets/sheild.png', '165px', '185px');
+newItem('assets/staff.png', '600px', '100px');
+
+document.body.style.backgroundImage = "url('assets/grass.png')"
+
+
